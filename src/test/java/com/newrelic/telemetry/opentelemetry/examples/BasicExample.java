a46aae6a4550626a92b298bf076e9ce3970d8e80
jwatson <jwatson@newrelic.com>
2020-03-05 10:34:22 -0800

    Update the instrumentation.provider to be more general. Also, make the sample code a bit more canonical, and update the README.

--- src/test/java/com/newrelic/telemetry/opentelemetry/examples/BasicExample.java
+++ src/test/java/com/newrelic/telemetry/opentelemetry/examples/BasicExample.java
@@ -2,13 +2,17 @@ package com.newrelic.telemetry.opentelemetry.examples;
 
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.opentelemetry.export.NewRelicSpanExporter;
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.trace.TracerSdkFactory;
 import io.opentelemetry.sdk.trace.export.BatchSpansProcessor;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Span.Kind;
 import io.opentelemetry.trace.Tracer;
 
 public class BasicExample {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     // NOTE: Under "normal" circumstances, you wouldn't use OpenTelemetry this way. This is just an
     // example to show how you can manually create a tracer for experimentation.
     // See the OpenTelemetry documentation at https://opentelemetry.io for more normal usage.
@@ -21,13 +25,27 @@ public class BasicExample {
             .build();
 
     // 2. Build the OpenTelemetry `BatchSpansProcessor` with the `NewRelicSpanExporter`
-    BatchSpansProcessor spansProcessor = BatchSpansProcessor.newBuilder(exporter).build();
+    BatchSpansProcessor spanProcessor = BatchSpansProcessor.newBuilder(exporter).build();
 
-    // 3. Add the span processor to a TracerSdkFactory
-    TracerSdkFactory tracerSdkFactory = TracerSdkFactory.create();
-    tracerSdkFactory.addSpanProcessor(spansProcessor);
+    // 3. Add the span processor to the default TracerSdkFactory
+    TracerSdkFactory tracerSdkFactory = (TracerSdkFactory) OpenTelemetry.getTracerFactory();
+    tracerSdkFactory.addSpanProcessor(spanProcessor);
 
     // 4. Create a OpenTelemetry `Tracer` and use it for recording spans.
-    Tracer tracer = tracerSdkFactory.get("sample-app", "1.0");
+    Tracer tracer = OpenTelemetry.getTracerFactory().get("sample-app", "1.0");
+
+    Span span = tracer.spanBuilder("testSpan").setSpanKind(Kind.INTERNAL).startSpan();
+    try (Scope scope = tracer.withSpan(span)) {
+      // do some work
+      Thread.sleep(1000);
+      span.end();
+    }
+
+    // clean up so the JVM can exit. Note: the spanProcessor will flush any spans to the exporter
+    // before it exits.
+    spanProcessor.shutdown();
+    // note: it shouldn't be necessary to explicitly shut down the exporter.
+    // See https://github.com/open-telemetry/opentelemetry-java/issues/965
+    exporter.shutdown();
   }
 }
