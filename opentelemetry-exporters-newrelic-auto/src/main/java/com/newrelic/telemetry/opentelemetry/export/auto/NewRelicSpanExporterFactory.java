f21ca73d10de8e792774d771fa9ad32f32843a38
Chandramouli S <xrma@nordstrom.com>
2020-05-05 15:13:53 -0700

    Add properties to enable audit logging and override uri

--- opentelemetry-exporters-newrelic-auto/src/main/java/com/newrelic/telemetry/opentelemetry/export/auto/NewRelicSpanExporterFactory.java
+++ opentelemetry-exporters-newrelic-auto/src/main/java/com/newrelic/telemetry/opentelemetry/export/auto/NewRelicSpanExporterFactory.java
@@ -2,15 +2,19 @@ package com.newrelic.telemetry.opentelemetry.export.auto;
 
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.opentelemetry.export.NewRelicSpanExporter;
+import io.opentelemetry.internal.StringUtils;
 import io.opentelemetry.sdk.contrib.auto.config.Config;
 import io.opentelemetry.sdk.contrib.auto.config.SpanExporterFactory;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import java.net.URI;
 
 public class NewRelicSpanExporterFactory implements SpanExporterFactory {
 
-  private static final String NEWRELIC_API_KEY = "newrelic.api.key";
-  private static final String NEWRELIC_SERVICE_NAME = "newrelic.service.name";
-  private static final String DEFAULT_NEWRELIC_SERVICE_NAME = "(unknown service)";
+  private static final String NEW_RELIC_API_KEY = "newrelic.api.key";
+  private static final String NEW_RELIC_ENABLE_AUDIT_LOGGING = "newrelic.enable.audit.logging";
+  private static final String NEW_RELIC_SERVICE_NAME = "newrelic.service.name";
+  private static final String NEW_RELIC_URI_OVERRIDE = "newrelic.uri.override";
+  private static final String DEFAULT_NEW_RELIC_SERVICE_NAME = "(unknown service)";
 
   /**
    * Creates an instance of a {@link SpanExporter} based on the provided configuration.
@@ -20,15 +24,26 @@ public class NewRelicSpanExporterFactory implements SpanExporterFactory {
    */
   @Override
   public SpanExporter fromConfig(Config config) {
-    final String apiKey = config.getString(NEWRELIC_API_KEY, "");
+    final String apiKey = config.getString(NEW_RELIC_API_KEY, "");
+    final boolean enableAuditLogging = config.getBoolean(NEW_RELIC_ENABLE_AUDIT_LOGGING, false);
     // todo: newrelic.service.name key will not required once service.name is provided via Resource
     // in the SDK
     final String serviceName =
-        config.getString(NEWRELIC_SERVICE_NAME, DEFAULT_NEWRELIC_SERVICE_NAME);
+        config.getString(NEW_RELIC_SERVICE_NAME, DEFAULT_NEW_RELIC_SERVICE_NAME);
+    final String uriOverride = config.getString(NEW_RELIC_URI_OVERRIDE, "");
 
-    return NewRelicSpanExporter.newBuilder()
-        .commonAttributes(new Attributes().put("service.name", serviceName))
-        .apiKey(apiKey)
-        .build();
+    NewRelicSpanExporter.Builder newRelicSpanExporterBuilder =
+        NewRelicSpanExporter.newBuilder()
+            .commonAttributes(new Attributes().put("service.name", serviceName))
+            .apiKey(apiKey);
+
+    if (enableAuditLogging) {
+      newRelicSpanExporterBuilder.enableAuditLogging();
+    }
+    if (!StringUtils.isNullOrEmpty(uriOverride)) {
+      newRelicSpanExporterBuilder.uriOverride(URI.create(uriOverride));
+    }
+
+    return newRelicSpanExporterBuilder.build();
   }
 }
--- opentelemetry-exporters-newrelic-auto/src/test/java/com/newrelic/telemetry/opentelemetry/export/auto/NewRelicSpanExporterFactoryTest.java
+++ opentelemetry-exporters-newrelic-auto/src/test/java/com/newrelic/telemetry/opentelemetry/export/auto/NewRelicSpanExporterFactoryTest.java
@@ -19,12 +19,17 @@ class NewRelicSpanExporterFactoryTest {
   void testFromConfig_HappyPath() {
     String apiKeyKey = "newrelic.api.key";
     String apiKeyValue = "test-key";
+    String enableAuditLoggingKey = "newrelic.enable.audit.logging";
     String defaultServiceName = "(unknown service)";
     String serviceNameKey = "newrelic.service.name";
     String serviceNameValue = "best service ever";
+    String uriOverrideKey = "newrelic.uri.override";
+    String uriOverrideValue = "http://test.domain.com";
 
     when(config.getString(apiKeyKey, "")).thenReturn(apiKeyValue);
+    when(config.getBoolean(enableAuditLoggingKey, false)).thenReturn(true);
     when(config.getString(serviceNameKey, defaultServiceName)).thenReturn(serviceNameValue);
+    when(config.getString(uriOverrideKey, "")).thenReturn(uriOverrideValue);
 
     NewRelicSpanExporterFactory newRelicSpanExporterFactory = new NewRelicSpanExporterFactory();
     SpanExporter spanExporter = newRelicSpanExporterFactory.fromConfig(config);
