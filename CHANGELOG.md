740ed790528a4ab057c8977629fdd76f092d91fa
jasonjkeller <jasonjkeller@gmail.com>
2020-12-09 18:22:53 -0800

    Update BasicExample and readme to be insync and support latest otel apis

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Coming soon
-- tbd
+[0.11.0] - 2020-12-nn
+- Updates compatibility with `io.opentelemetry:opentelemetry-sdk:0.11.0`
+- Updates compatibility with `io.opentelemetry.javaagent:opentelemetry-javaagent-spi:0.11.0`
 
 ## [0.10.0] - 2020-11-13
 - Updates compatibility with `io.opentelemetry:opentelemetry-sdk:0.10.0`
--- opentelemetry-exporters-newrelic/src/test/java/com/newrelic/telemetry/opentelemetry/examples/BasicExample.java
+++ opentelemetry-exporters-newrelic/src/test/java/com/newrelic/telemetry/opentelemetry/examples/BasicExample.java
@@ -6,7 +6,6 @@
 package com.newrelic.telemetry.opentelemetry.examples;
 
 import com.newrelic.telemetry.opentelemetry.export.NewRelicExporters;
-import com.newrelic.telemetry.opentelemetry.export.NewRelicExporters.Configuration;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Labels;
 import io.opentelemetry.api.metrics.LongCounter;
@@ -19,27 +18,38 @@ import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
 import java.util.Random;
 
+/**
+ * An example illustrating common usage of the New Relic OpenTelemetry exporter. This example walks
+ * through using OpenTracing APIs/SDKs to manually instrument application code and setting up the
+ * New Relic OpenTelemetry exporter for reporting OpenTelemetry trace and metric data to New Relic.
+ */
 public class BasicExample {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
     String apiKey = System.getenv("INSIGHTS_INSERT_KEY");
 
-    // 1. The simplest way to configure the New Relic exporters is like this:
-    Configuration configuration =
-        new Configuration(apiKey, "best service ever")
-            .enableAuditLogging()
-            .collectionIntervalSeconds(10);
+    // Configure the New Relic exporters.
+    NewRelicExporters.Configuration configuration =
+        new NewRelicExporters.Configuration(apiKey, "My Service Name")
+            .enableAuditLogging() // Optionally enable audit logging.
+            .collectionIntervalSeconds(
+                10); // Set the reporting interval for metrics/spans to 10 seconds.
+
+    // Start the exporters with the supplied configuration. This starts both a NewRelicSpanExporter
+    // and a NewRelicMetricExporter as well as a BatchSpanProcessor and an IntervalMetricReader, the
+    // latter of which manage the batching and sending of their respective telemetry data types.
     NewRelicExporters.start(configuration);
 
-    // Now, we've got the SDK configured and the exporters started.
-    // Let's write some very simple instrumentation to demonstrate how it all works.
+    // Now that we've got the SDK configured and the exporters started, let's write some very simple
+    // instrumentation to demonstrate how it all works.
 
-    // 2. Create an OpenTelemetry `Tracer` and a `Meter` and use them for some manual
-    // instrumentation.
+    // Call the OpenTelemetry SDK to obtain tracers and meters to record data.
+    // A Tracer is used to create Spans that form traces.
     Tracer tracer = OpenTelemetry.getGlobalTracerProvider().get("sample-app", "1.0");
+    // A Meter is used to create different instruments to record metrics.
     Meter meter = OpenTelemetry.getGlobalMeterProvider().get("sample-app", "1.0");
 
-    // 3. Here is an example of a counter
+    // Use the meter to create a LongCounter instrument to record metrics.
     LongCounter spanCounter =
         meter
             .longCounterBuilder("spanCounter")
@@ -47,7 +57,7 @@ public class BasicExample {
             .setDescription("Counting all the spans")
             .build();
 
-    // 4. Here's an example of a measure.
+    // Use the meter to create a LongValueRecorder instrument to record metrics.
     LongValueRecorder spanTimer =
         meter
             .longValueRecorderBuilder("spanTimer")
@@ -55,22 +65,23 @@ public class BasicExample {
             .setDescription("How long the spans take")
             .build();
 
+    // Use the meter to create a LongUpDownCounter instrument to record metrics.
     LongUpDownCounter upDownCounter =
         meter
-            .longUpDownCounterBuilder("jim")
-            .setDescription("some good testing")
+            .longUpDownCounterBuilder("upDownCounter")
+            .setDescription("Counter that can sum negative values")
             .setUnit("1")
             .build();
 
-    // 5. Optionally, you can pre-bind a set of labels, rather than passing them in every time.
+    // Optionally, you can pre-bind a set of labels, rather than passing them in every time.
     LongValueRecorder.BoundLongValueRecorder boundTimer =
         spanTimer.bind(Labels.of("spanName", "testSpan"));
 
-    // 6. use these to instrument some work
+    // Use the instruments to record metric measurements and the tracer for spans.
     doSomeSimulatedWork(tracer, spanCounter, upDownCounter, boundTimer);
 
-    // clean up so the JVM can exit. Note: these will flush any data to the exporter
-    // before they exit.
+    // When the application is complete, be sure to call shutdown to stop the exporters.
+    // This will flush any data from the exporters before they exit.
     NewRelicExporters.shutdown();
   }
 
@@ -78,24 +89,36 @@ public class BasicExample {
       Tracer tracer,
       LongCounter spanCounter,
       LongUpDownCounter upDownCounter,
-      LongValueRecorder.BoundLongValueRecorder boundTimer)
-      throws InterruptedException {
+      LongValueRecorder.BoundLongValueRecorder boundTimer) {
     Random random = new Random();
+
     for (int i = 0; i < 10; i++) {
       long startTime = System.currentTimeMillis();
       Span span =
           tracer.spanBuilder("testSpan").setSpanKind(Span.Kind.INTERNAL).setNoParent().startSpan();
-      try (Scope ignored = span.makeCurrent()) {
+
+      // Start timing for the span
+      try (Scope scope = span.makeCurrent()) {
         boolean markAsError = random.nextBoolean();
+
         if (markAsError) {
           span.setStatus(StatusCode.ERROR, "internalError");
         }
+
+        // Use the instruments to record measurements
         spanCounter.add(1, Labels.of("spanName", "testSpan", "isItAnError", "" + markAsError));
         upDownCounter.add(random.nextInt(100) - 50);
-        // do some work
+
+        // Do some busy waiting work
         Thread.sleep(random.nextInt(1000));
-        span.end();
+
+        // Use the boundTimer instrument to record another measurement
         boundTimer.record(System.currentTimeMillis() - startTime);
+      } catch (Throwable t) {
+        span.setStatus(StatusCode.ERROR, "error description"); // record error details
+      } finally {
+        // End timing for the span
+        span.end(); // closing the scope does not end the span, this has to be done manually
       }
     }
   }
